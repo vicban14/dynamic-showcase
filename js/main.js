427f7fe15a7d362fa602dfff89ddb8cfd7c7c61a
Victor Bañuls <vicban@MacBook-Pro-de-Victor.local>
2020-12-23 19:21:39 +0100

    Add dropable in bowl elements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 'use strict'
 
-var menuImages = document.querySelectorAll('.menu-container > div > img')
+const menuImages = document.querySelectorAll('.menu-container > div > img')
 menuImages.forEach(image => {
   image.setAttribute("draggable", "true")
   image.setAttribute("ondragstart", "drag(event)")
 })
+
+const bowls = document.querySelectorAll('.space-bowl')
+bowls.forEach(bowl => {
+  bowl.setAttribute("ondrop", "drop(event)")
+  bowl.setAttribute("ondragover", "allowDrop(event)")
+})
+
+const tray = document.querySelectorAll('.tray')[0]
+tray.setAttribute("ondrop", "drop(event)")
+tray.setAttribute("ondragover", "allowDrop(event)")
 
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id)
